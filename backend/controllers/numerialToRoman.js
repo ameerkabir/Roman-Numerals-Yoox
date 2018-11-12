a03d4265cbe9b69711fde3a32d3b1f4937e090d5
ameerkabir <ameerkabir2@gmail.com>
2018-11-12 11:34:35 +0000

    removed extra stuff

--- backend/controllers/numerialToRoman.js
+++ backend/controllers/numerialToRoman.js
@@ -2,13 +2,6 @@ const { converToRoman } = require("../helpers/index");
 // const { isNumber } = require("../helpers/utill");
 const numericalToRoman = async (req, res, next) => {
   const { number } = req.query;
-  // if (!isNumber(Number(number))) {
-  //   res.status(400).json({
-  //     success: false,
-  //     message: "Please choose a number between 1 and 3999",
-  //     number
-  //   });
-  // }
   try {
     const roman = await converToRoman(number);
     res.status(200).json({
