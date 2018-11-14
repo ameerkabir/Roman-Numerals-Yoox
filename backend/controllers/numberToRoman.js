f46e29e9208aacb85ad36acc80f437766612f9ba
ameerkabir <ameerkabir2@gmail.com>
2018-11-14 18:05:57 +0000

    changed layout from flex-box to css grid

---
+++ backend/controllers/numberToRoman.js
@@ -0,0 +1,20 @@
+const { converToRoman } = require("../helpers/index");
+// const { isNumber } = require("../helpers/utill");
+const numberToRoman = async (req, res, next) => {
+  const { number } = req.query;
+  try {
+    const roman = await converToRoman(number);
+    res.status(200).json({
+      success: true,
+      number,
+      roman
+    });
+  } catch (err) {
+    return res.status(502).json({
+      status: false,
+      message: "Something went wrong",
+      err
+    });
+  }
+};
+module.exports = numberToRoman;
