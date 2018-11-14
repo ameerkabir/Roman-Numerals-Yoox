8f64d7b4618d6bd496414ca2a66bf21106c8fdcc
ameerkabir <ameerkabir2@gmail.com>
2018-11-14 18:02:52 +0000

    modeived controlers madules

---
+++ backend/controllers/romanToNumber.js
@@ -0,0 +1,27 @@
+const { converToInteger , isRoman} = require("../helpers/index");
+
+const romanToNumber = async (req, res) => {
+  const { roman } = req.query;
+  if (!isRoman(roman)) {
+    res.status(400).json({
+      succes: false,
+      message: "Pleas provide valid Roman number to continue",
+      roman
+    });
+  }
+  const number = await converToInteger(roman);
+  try {
+    res.status(200).json({
+      success: true,
+      roman,
+      number
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+      err
+    });
+  }
+};
+module.exports = romanToNumber;
