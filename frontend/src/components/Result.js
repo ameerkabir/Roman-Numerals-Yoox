7fa7aeb06a347633d6070281a70080c7ae4610de
ameerkabir <ameerkabir2@gmail.com>
2018-11-16 18:20:33 +0000

    fixed display issue

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -1,6 +1,9 @@
 import React from "react";
-const Result = ({ Toshow, roman, integer }) => {
-  console.log(Toshow, roman)
-  return <div className="result">{ roman ?<div> {roman}</div> : <div>{integer}</div>}</div>;
+const Result = ({ ToShow, roman, integer }) => {
+  return (
+    <div className="result">
+      {ToShow ? <div> {roman}</div> : <div>{integer}</div>}
+    </div>
+  );
 };
 export default Result;
